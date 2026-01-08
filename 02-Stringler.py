c6774936458cc96581c960f13d8ea2bd942df6e6
ramazan <coolstoves@gmail.com>
2026-01-08 03:30:22 +0300

    08012026

--- 02-Stringler.py
+++ 02-Stringler.py
@@ -2,3 +2,15 @@ metin="Merhaba Dunya!!!!"
 yeniDefgisken=metin.split()
 print(yeniDefgisken)
 print(type(yeniDefgisken))
+print(metin.upper)
+
+
+
+print(metin.replace("M","Y"))
+
+#Class içinde olmayan metodlara fonksiyon denir.
+#metodlarla fonksiyonlar arasındaki fark nedir?
+#metodlar class içinde tanımlanır.
+#Script dillerde class yapısı zorunlu değildir.
+
+#Python'da herşey bir classdır.
