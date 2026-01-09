7e3894507b0ccb13dbf067a7ff29d04d250e686f
ramazan <coolstoves@gmail.com>
2026-01-09 16:24:12 +0300

    09012026

---
+++ 06-Tuple.py
@@ -0,0 +1,2 @@
+myTuple=("Yusuf",42,["Tuple icindeki liste 1","Tuple icindeki liste 2"])
+print(type(myTuple))
---
+++ 07-Dictionary.py
@@ -0,0 +1,31 @@
+myDictionary={
+"name":"Yusuf",
+"Yas":39,
+"durum":True
+}
+
+print(myDictionary)
+
+insanlar={
+
+"id1" :{
+
+    "name" :"Yusuf",
+    "yas" : 39
+},
+
+"id2":{
+
+    "name": "Ahmet",
+    "yas":40,
+}
+
+}
+
+print(insanlar)
+
+myDictionary["yeniKey"]="Buda onu degeri"
+print(myDictionary)
+
+
+
