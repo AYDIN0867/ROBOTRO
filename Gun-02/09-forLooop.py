6d0bec6cf0c678f687c6eb28fa631dab04ce6b16
ramazan <coolstoves@gmail.com>
2026-01-11 17:39:15 +0300

    11012026

---
+++ Gun-02/09-forLooop.py
@@ -0,0 +1,16 @@
+
+
+isimler=["Yusuf","Ahmet","Akif","Ayşe"]
+for e in isimler:
+    print(e)
+
+
+isimler.sort()
+print(isimler)
+
+for index, eleman in enumerate (isimler):
+    print(f"{index}:{eleman}")
+
+for i in range(0,10):
+    print(i, end=" ")
+
