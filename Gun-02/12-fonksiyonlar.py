6a3ec96fde630fe33a27f50f59b7af8ee2614226
ramazan <coolstoves@gmail.com>
2026-01-14 15:33:24 +0300

    14012026

--- Gun-02/12-fonksiyonlar.py
+++ Gun-02/12-fonksiyonlar.py
@@ -1,4 +1,25 @@
 def ekranaMerhabaYaz():
     print("Merhaba")
 
-ekranaMerhabaYaz()
+ekranaMerhabaYaz() #Merhaba
+
+def ekranaYaz(cumle):
+    print("Merhaba "+cumle)
+
+ekranaYaz("Ramazan")#Merhaba Ramazan
+
+def karesiAl(sayi:int)-> int:
+    return sayi **2
+print (karesiAl(123)) #15129 Tab scope u belirler ve çok önemlidir 
+#Dikkat edilmelidir...
+
+
+
+def defaultParametreAlanFonksiyon(isim="ramazan"):
+    print(f"Merhaba {isim}")
+defaultParametreAlanFonksiyon("Yusuf") #Merhaba Yusuf
+defaultParametreAlanFonksiyon()
+
+
+
+
